c053e20b04c34476472775836e3fa4fc86b97562
Caio Henrique <caiohenriquepat@gmail.com>
2025-03-31 20:49:13 -0300

    Revert "Boa sorte"
    This reverts commit c2aa326920f2d84e0566d7f9c4068c2b54fb75ac.

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,12 +2,11 @@ import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
  
 export default function Dashboard() {
- 
   return (
     <View style={{ flex: 1, alignitems:'center',justifycontent:'center'}}>
       <Text>Dashboard</Text>
 
-      <Link href="/" style={{ margin: 10, color:'blue' }}>
+      <Link href="../dashboard/index" style={{ margin: 10, color:'blue' }}>
         <Text>Voltar para login</Text>
       </Link>
          
